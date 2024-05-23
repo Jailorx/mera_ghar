24094434f0c47044a4aa59994806b049927162cb
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-05-23 16:43:12 +0530

    Add reusable Card Components in homepage

---
+++ src/components/HomePage/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = ({ title, content }) => {
+  return (
+    <article className="flex flex-col w-64 h-40 items-center justify-center p-4 shadow-xl rounded-md hover:scale-110">
+      <h1 className="font-bold text-4xl text-primary-custom-indigo-600">
+        {title}
+      </h1>
+      <p className="text-xl">{content}</p>
+    </article>
+  );
+};
+
+export default Card;
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "./Card/Card";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,11 @@ const HomePage = () => {
           Your go-to hub for effortlessly buying, selling, or renting homes and
           flats. Experience real estate made easy, fast, and reliable!"
         </p>
+      </section>
+      <section className="flex flex-col sm:flex-row gap-5 p-5 w-full justify-center items-center">
+        <Card title="5000+" content="Verified Customers" />
+        <Card title="5+" content="Years of Experience" />
+        <Card title="1000+" content="Satisfied customers" />
       </section>
     </main>
   );
