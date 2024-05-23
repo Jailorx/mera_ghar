a1fa20ad7454831daab0eb88d64301401e551d77
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-05-23 16:27:37 +0530

    Add HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   return (
     <div className="w-full mx-auto md:px-20">
       <Header />
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main className="flex flex-col w-full p-10">
+      <section>
+        <h1 className="font-bold  text-primary-custom-indigo-700 text-5xl leading-snug">
+          Find your dream home
+        </h1>
+        <p className="mt-4 text-3xl leading-normal">
+          Your go-to hub for effortlessly buying, selling, or renting homes and
+          flats. Experience real estate made easy, fast, and reliable!"
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
